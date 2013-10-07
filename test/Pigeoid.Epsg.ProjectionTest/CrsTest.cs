707dfa0230403346ba4488a492da1a08727e7e22
Aaron Dandy <aaron.dandy@live.com>
2013-10-07 17:18:03 -0600

    starting over on the test app

--- test/Pigeoid.Epsg.ProjectionTest/CrsTest.cs
+++ test/Pigeoid.Epsg.ProjectionTest/CrsTest.cs
@@ -22,84 +22,28 @@ namespace Pigeoid.Epsg.ProjectionTest
             if (IgnoreDeprecated)
                 items = items.Where(x => !x.Deprecated);
 
-            var itemsList = items.ToList();
-            foreach (var fromCrs in itemsList) {
+            // ReSharper disable PossibleMultipleEnumeration
+            foreach (var fromCrs in items) {
                 var fromArea = fromCrs.Area;
-                foreach (var toCrs in itemsList) {
+                foreach (var toCrs in items) {
                     if (toCrs != fromCrs && fromArea.Intersects(toCrs.Area)) {
                         yield return new CrsTestCase(fromCrs, toCrs);
                     }
                 }
             }
-        }
-
-        public IEnumerable<T[]> Batch<T>(List<T> items, int batchSize) {
-            for (int i = 0; i < items.Count; i += batchSize) {
-                int startIndex = i;
-                int endIndex = startIndex + batchSize;
-                if (endIndex > items.Count) {
-                    endIndex = items.Count;
-                }
-                var batch = new T[endIndex - startIndex];
-                if (batch.Length > 0) {
-                    items.CopyTo(startIndex, batch, 0, batch.Length);
-                    yield return batch;
-                }
-            }
+            // ReSharper restore PossibleMultipleEnumeration
         }
 
         public void Run(Action<IEnumerable<CrsTestCase>> saveResults) {
-            var batchBufferSize = 8;
-            var batchSize = 512;
+            var crsTests = GenerateAreaIntersectingCrs();
+            var graphFilter = IgnoreDeprecated
+                ? new EpsgCrsCoordinateOperationPathGenerator.SharedOptionsAreaPredicate(x => !x.Deprecated, x => !x.Deprecated)
+                : new EpsgCrsCoordinateOperationPathGenerator.SharedOptionsAreaPredicate(x => true, x => true);
 
-            Console.Write("Generating test cases...");
-            var crsTestList = GenerateAreaIntersectingCrs().ToList();
-            Console.WriteLine("done.");
-
-            Console.WriteLine("Beginning Tests");
-
-            var executedTestsBuffer = new BlockingCollection<CrsTestCase[]>(batchBufferSize);
-            var taskFactory = new TaskFactory(TaskCreationOptions.LongRunning, TaskContinuationOptions.None);
-            var writeResultsTask = taskFactory.StartNew(() => saveResults(executedTestsBuffer.GetConsumingEnumerable().SelectMany(x => x)));
-
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            var pathGenerator = new EpsgCrsCoordinateOperationPathGenerator(new EpsgCrsCoordinateOperationPathGenerator.SharedOptionsAreaPredicate(x => !x.Deprecated, x => !x.Deprecated));
-
+            var pathGenerator = new EpsgCrsCoordinateOperationPathGenerator(graphFilter);
             var transformGenerator = new StaticCoordinateOperationCompiler();
 
-            int processedItems = 0;
-            foreach (var batch in Batch(crsTestList, batchSize)) {
-                Console.WriteLine(
-                    "{0:P6} ({1}/{2}) @{3}ops/s",
-                    (processedItems / (double)crsTestList.Count),
-                    processedItems,
-                    crsTestList.Count,
-                    processedItems / (stopwatch.ElapsedMilliseconds / 1000.0)
-                );
-                Parallel.ForEach(batch, testCase => {
-                    try {
-                        testCase.Run(pathGenerator, transformGenerator);
-                    }
-                    catch (Exception ex) {
-                        Console.WriteLine("Failure on '{0}'.", testCase);
-                        Console.WriteLine(ex);
-                    }
-                });
-                processedItems += batch.Length;
-                executedTestsBuffer.Add(batch);
-            }
-
-            stopwatch.Stop();
-            Console.WriteLine("All tests complete");
-            Console.WriteLine("Execution took: {0}", stopwatch.Elapsed);
-
-            Console.Write("Completing report generation...");
-            executedTestsBuffer.CompleteAdding();
-            Task.WaitAll(writeResultsTask);
-            Console.WriteLine("done!");
-
+            throw new NotImplementedException();
         }
 
 
