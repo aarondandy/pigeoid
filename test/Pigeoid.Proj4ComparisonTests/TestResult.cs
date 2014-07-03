297bf70d0b510affc63dbb0e24ff3d1f889d00cc
Aaron Dandy <aaron.dandy@live.com>
2014-07-03 17:10:29 -0600

    left the test result class out

---
+++ test/Pigeoid.Proj4ComparisonTests/TestResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pigeoid.Proj4ComparisonTests
+{
+    public class TestResult
+    {
+
+        public ICrs Source { get; set; }
+        public ICrs Target { get; set; }
+
+        public CoordinateComparisonStatistics StatsData { get; set; }
+
+    }
+}
