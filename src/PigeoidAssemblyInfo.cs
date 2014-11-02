e6e3b4eca930148763189b31b6bd0e8d596ce4e0
Aaron Dandy <aaron.dandy@live.com>
2014-11-01 19:08:35 -0600

    created a crappy nuget build... it works...

--- src/PigeoidAssemblyInfo.cs
+++ src/PigeoidAssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("0.1.0.*")]
